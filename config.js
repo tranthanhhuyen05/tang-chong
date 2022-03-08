c59dde5a0ab876f5ca3687c5a596804d2cc10336
Halyn Tran <86349680+tranthanhhuyen05@users.noreply.github.com>
2022-03-08 13:18:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Chồng yêu à à!',
+    introTitle: 'Chồng yêu àaaaaa!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao anh cứ đi nhầm đường
     Lạc vào tim em lẻ loi
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu chồng nhiều nhiều 😘😘',
+    mess: 'Em biết mà 🥰 Yêu chồng nhiều nhiều 😘😘',
     messDesc: 'Yêu chồng nhiều lắmmmmmm.',
     btnAccept: 'Yêu vợ <3',
     messLink: 'https://www.facebook.com/tranthanhhuyen05/' //link mess của các bạn. VD: https://m.me/nam.nodemy
