02a56275d5f80ace36528669cb3749d81eda4342
Halyn Tran <86349680+tranthanhhuyen05@users.noreply.github.com>
2022-03-08 17:54:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Chồng yêu àaaaaa!',
+    introTitle: 'Em yêu àaaaaa!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Yêu vợ <33',
-    btnNo: 'Không, yêu conkhac',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
-    btnReply: 'Gửi cho em <3',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu anh <33',
+    btnNo: 'Không, anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰 Yêu chồng nhiều nhiều 😘😘',
-    messDesc: 'Yêu chồng nhiều lắmmmmmm',
-    btnAccept: 'Yêu vợ <3',
+    mess: 'Anh biết mà 🥰 Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối 7h anh qua rước công chúa nhé',
+    btnAccept: 'Yêu anh <3',
     messLink: 'https://www.facebook.com/tranthanhhuyen05/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
